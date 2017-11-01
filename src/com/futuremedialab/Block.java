692289baafb5b32516cae56db989186d317f58c0
stan-is-laus <stanguldemond@gmail.com>
2017-11-01 14:16:15 +0100

    first edit constructor

--- src/com/futuremedialab/Block.java
+++ src/com/futuremedialab/Block.java
@@ -14,7 +14,7 @@ public class Block {
     private int previousHash;
     private String[] transactions;
 
-    public Block(int previousHash, String[] transactions) {
+    public Block(int index, Date timestamp, int previousHash, String[] transactions) {
         this.previousHash = previousHash;
         this.transactions = transactions;
 
