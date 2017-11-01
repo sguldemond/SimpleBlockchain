4f6f1a174375ed9afa4517299853b2f9f33d9c1e
stan-is-laus <stanguldemond@gmail.com>
2017-11-01 20:31:12 +0100

    attempt to write json to file

--- src/main/java/webservice/PostHandler.java
+++ src/main/java/webservice/PostHandler.java
@@ -4,10 +4,7 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 
 /**
  * Created by stasiuz on 01/11/2017.
@@ -27,13 +24,24 @@ public class PostHandler implements HttpHandler {
 
         System.out.printf(message.toString());
 
-        JSONObject json = new JSONObject(message.toString());
+        JSONObject transaction = new JSONObject(message.toString());
+
+        File node = new File("node.json");
+        FileWriter file;
+        if(node.exists()) {
+            file = new FileWriter(node);
+        } else {
+            file = new FileWriter("node.json");
+        }
+
+        file.append(transaction.toString());
+        file.flush();
 
 
         StringBuilder response = new StringBuilder();
-        response.append(String.format("FROM: %s \n", json.get("from")));
-        response.append(String.format("TO: %s \n", json.get("to")));
-        response.append(String.format("AMOUNT: %s", json.get("amount")));
+        response.append(String.format("FROM: %s \n", transaction.get("from")));
+        response.append(String.format("TO: %s \n", transaction.get("to")));
+        response.append(String.format("AMOUNT: %s", transaction.get("amount")));
 
         httpExchange.sendResponseHeaders(200, response.length());
         OutputStream os = httpExchange.getResponseBody();
--- src/main/java/webservice/Server.java
+++ src/main/java/webservice/Server.java
@@ -6,12 +6,26 @@ import com.sun.net.httpserver.HttpServer;
 import org.json.JSONObject;
 import java.io.*;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
 
 /**
  * Created by stasiuz on 01/11/2017.
  */
 public class Server {
-    public static void main(String[] args) throws IOException {
+
+    public ArrayList<JSONObject> nodeTransactions = new ArrayList<>();
+
+    public static void main(String[] args) {
+        try {
+            start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void start() throws IOException {
         int port = 9000;
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         System.out.println("Server started at port: " + port);
